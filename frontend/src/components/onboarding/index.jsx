bdfbcd172ab99717d4ce51d35b835fce49b5f547
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-03-16 03:22:25 -0400

    Add index file for onboarding components

---
+++ frontend/src/components/onboarding/index.jsx
@@ -0,0 +1,6 @@
+// Export all onboarding-related components
+export { default as Onboarding } from './Onboarding';
+export { default as AthleteProfile } from './AthleteProfile';
+export { default as GoalSetting } from './GoalSetting';
+export { default as TrainingPreferences } from './TrainingPreferences';
+export { default as InitialAssessment } from './InitialAssessment';
