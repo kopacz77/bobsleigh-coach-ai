bd926333a21a90ecc66ac1666035b7b3fe7fb6b8
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-03-16 02:20:10 -0400

    Add performance module index file

---
+++ frontend/src/components/performance/index.jsx
@@ -0,0 +1,4 @@
+// Export all performance-related components
+export { default as PerformanceAssessment } from './PerformanceAssessment';
+export { default as PerformanceMetrics } from './PerformanceMetrics';
+export { default as PerformanceTrends } from './PerformanceTrends';
