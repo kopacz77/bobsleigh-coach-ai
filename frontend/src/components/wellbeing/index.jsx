1c9781ca1e5fbfc7519d2c054e0cf916ce04be9b
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-03-16 00:06:48 -0400

    Add wellbeing module index file

---
+++ frontend/src/components/wellbeing/index.jsx
@@ -0,0 +1,6 @@
+// Export all wellbeing-related components
+export { default as WellbeingAssessment } from './WellbeingAssessment';
+export { default as MoodTracking } from './MoodTracking';
+export { default as PhysicalMetrics } from './PhysicalMetrics';
+export { default as Reflection } from './Reflection';
+export { default as RecoveryHealth } from './RecoveryHealth';
